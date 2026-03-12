b9a45683cfdc81e419fef41741a986246d9f5eab
Filip Farley <filip@farley.se>
2026-03-12 14:58:30 +0100

    Updated OpenAQ-API, filtered 10 active stations in vicinity of Stockholm/Arlanda to get data within 24 hours. Cleaned data

--- OpenAQ-fetcher/openaq_fetcher.py
+++ OpenAQ-fetcher/openaq_fetcher.py
@@ -1,5 +1,6 @@
 import requests
 from fastapi import FastAPI
+from datetime import datetime, timezone, timedelta
 from openaq_API_KEY import OPENAQ_API_KEY
 
 app = FastAPI()
@@ -7,15 +8,85 @@ app = FastAPI()
 @app.get("/air-quality")
 def get_air_quality():
     
-    url = "https://api.openaq.org/v3/locations/8118"
+    url = "https://api.openaq.org/v3/locations"
     
     headers = {
         "X-API-Key": OPENAQ_API_KEY
     }
     
-    response = requests.get(url, headers=headers)
+    params = {
+        "bbox": "17.5,59.2,18.5,59.8",
+        "limit": 50 # Hämtar max 5 luftkvalitet stationer för att testa.
+    }
     
-    if response.status_code == 200:
-        return response.json()
-    else:
+    
+    response = requests.get(url, headers=headers, params=params)   
+    
+    if response.status_code != 200:
         return {"error": "Kunde inte hämta luftdata", "status_code": response.status_code}
+    
+    locations_data = response.json().get("results", [])
+    
+    # Filtrera fram 10 Aktiva stationer (Uppdaterade data inom senaste 24 timmarna)
+    
+    active_stations = []
+    now = datetime.now(timezone.utc)
+    
+    for location in locations_data:
+        last_updated_str = location.get("datetimeLast", {}).get("utc")
+        
+        if last_updated_str:
+            last_updated_dt = datetime.fromisoformat(last_updated_str.replace("Z", "+00:00"))
+            
+            if now - last_updated_dt < timedelta(hours=24):
+                active_stations.append(location)
+                
+        if len (active_stations) == 10:
+            break
+        
+    dashboard_data = []
+    
+    for station in active_stations:
+        station_id = station["id"]
+        
+        sensor_map = {}
+        for sensor in station.get("sensors", []):
+            sensor_map[sensor["id"]] = {
+                "parameter": sensor["parameter"]["name"],
+                "units": sensor["parameter"]["units"]
+            }
+        
+        url_latest = f"https://api.openaq.org/v3/locations/{station_id}/latest"
+        latest_response = requests.get(url_latest, headers=headers)
+        
+        measurements = []
+        if latest_response.status_code == 200:
+            
+            latest_data = latest_response.json().get("results", [])
+            
+            for m in latest_data:
+                sensor_id = m.get("sensorsId")
+                value = m.get("value")
+                
+                sensor_info = sensor_map.get(sensor_id)
+                if sensor_info:
+                    measurements.append({
+                        "parameter": sensor_info["parameter"],
+                        "value": value,
+                        "units": sensor_info["units"]
+                    })
+                    
+        dashboard_data.append({
+            "station_name": station["name"],
+            "city": station.get("locality", "Okänd"),
+            "latitude": station["coordinates"]["latitude"],
+            "longitude": station["coordinates"]["longitude"],
+            "last_updated": station["datetimeLast"]["local"],
+            "measurements": measurements
+        })
+        
+    return {
+        "status": "success",
+        "active_stations_found": len(dashboard_data),
+        "data": dashboard_data
+    }
